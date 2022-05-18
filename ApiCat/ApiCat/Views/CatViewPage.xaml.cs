b868465979f49532322dfc4bdbb5478b6d7dacf4
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-05-18 20:07:57 +0300

    createcatpage and catviewpage

--- ApiCat/ApiCat/Views/CatViewPage.xaml.cs
+++ ApiCat/ApiCat/Views/CatViewPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApiCat.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,25 @@ namespace ApiCat.Views
         {
             base.OnAppearing();
             listView.ItemsSource = await App.CountManager.GetTasksAsync();
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CreateCatPage(true)
+            {
+                BindingContext = new CatModel()
+                {
+                    id = Guid.NewGuid().ToString(),
+                }
+            });
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Navigation.PushAsync(new CreateCatPage()
+            {
+                BindingContext = e.SelectedItem as CatModel
+            });
         }
     }
 }
---
+++ ApiCat/ApiCat/Views/CreateCatPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ApiCat.Model;
+using ApiCat.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ApiCat.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateCatPage : ContentPage
+    {
+        bool isNewItem;
+        public CreateCatPage(bool isNew = false)
+        {
+            InitializeComponent();
+            isNewItem = isNew;
+        }
+
+        private async void btn_Save_ClickedAsync(object sender, EventArgs e)
+        {
+            var Item = (CatModel)BindingContext;
+            await App.CountManager.SaveItemAsync(Item, isNewItem);
+            await Navigation.PopAsync();
+        }
+
+        private async void btn_Delete_Clicked(object sender, EventArgs e)
+        {
+            var Item = (CatModel)BindingContext;
+            await App.CountManager.DeleteTodoAsync(Item);
+            await Navigation.PopAsync();
+        }
+    }
+}
